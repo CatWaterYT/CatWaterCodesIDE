ae37036ef5135c805201d023a65ddb69b2aa67c4
jasongionis1012@gmail.com <jasongionis1012@gmail.com>
2024-10-25 16:29:22 +0300

    Add project files.

---
+++ FileManagement.cs
@@ -0,0 +1,59 @@
+﻿using Dragablz;
+using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Web.WebView2.Wpf;
+using System.Windows.Input;
+using System.IO;
+using System;
+
+namespace CatWaterCodes_IDE {
+    static internal class FileManagement {
+
+        public static void SaveFile(string path, string content) {
+            if (File.Exists(path)) {
+                File.WriteAllText(path, content);
+            }
+
+            else {
+                WriteToNewFile(content);
+            }
+        }
+        public static void WriteToNewFile(string content) {
+            SaveFileDialog saveFile = new SaveFileDialog();
+
+            if(saveFile.ShowDialog() != true) {
+                return;
+            }
+
+            File.WriteAllText(saveFile.FileName, content);
+        }
+
+        public static void CreateEmptyFile(Window main) {
+            
+            TabItem scriptTab = CreateTab(main);
+            TextBox tb = new TextBox();
+            tb.VerticalScrollBarVisibility = ScrollBarVisibility.Visible; tb.AcceptsReturn = true; tb.AcceptsTab = true; tb.Text = " a";
+            scriptTab.Content = tb;
+            scriptTab.IsSelected = true;
+        }
+        public static void OpenWebsiteInNewTab(Window main, string url="https://google.com", string name="browser") {
+            TabItem browserTab = CreateTab(main);
+            browserTab.Header = name;
+            WebView2 browser = new WebView2();
+            Uri address = new Uri(url);
+            browser.Source = address;
+            browserTab.Content = browser;
+            
+        }
+
+        public static TabItem CreateTab(Window main) {
+            TabablzControl tabCo = (TabablzControl)main.FindName("InitialTabablzControl");
+            TabItem newTab = new TabItem();
+            newTab.Header = "<null>*"; 
+            tabCo.Items.Add(newTab);
+            //tabCo.MouseDown += new MouseButtonEventHandler(MainWindow.TabClicked);
+            return newTab;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using Dragablz;
+
+namespace CatWaterCodes_IDE {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        static string path = "K:/file.txt";
+        //public static TabablzControl selectedTab;
+
+        public MainWindow() {
+            InitializeComponent();
+            //selectedTab = (TabablzControl)this.FindName("InitialTabablzControl");
+            Debug.WriteLine("hello world");
+        }
+
+        void NewFileBtn(object sender, RoutedEventArgs e) {
+            FileManagement.CreateEmptyFile(this);
+        }
+
+        void SaveBtn(object sender, RoutedEventArgs e) {
+            TabablzControl tabablzControl = (Dragablz.TabablzControl)this.FindName("MainTabControl");
+            TextBox tb = (TextBox)tabablzControl.SelectedContent;
+            string content = tb.Text;
+
+            FileManagement.SaveFile(path, content);
+        }
+
+        void SaveAsBtn(object sender, RoutedEventArgs e) {
+            TabablzControl tabablzControl = (Dragablz.TabablzControl)this.FindName("MainTabControl");
+            TextBox tb = (TextBox)tabablzControl.SelectedContent; 
+            string content = tb.Text;
+
+            FileManagement.WriteToNewFile(content);
+        }
+
+        void NewBrowser(object sender, RoutedEventArgs e) {
+            FileManagement.OpenWebsiteInNewTab(this);
+        }
+
+        void NewOpenURL(object sender, RoutedEventArgs e) {
+            Button clicked = sender as Button;
+            string url = clicked.Tag.ToString();
+            string name = clicked.Content.ToString();
+
+            FileManagement.OpenWebsiteInNewTab(this, url, name);
+        }
+        /*public static void TabClicked(object sender, RoutedEventArgs e) {
+            TabablzControl clickedTab = sender as TabablzControl;
+            Debug.WriteLine("mememe");
+            if (clickedTab == selectedTab || clickedTab == null) return;
+            selectedTab = clickedTab;
+        }*/
+    }
+}
